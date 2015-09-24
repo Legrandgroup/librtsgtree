2d266a602f8ada853e7212cc34c959649100d003
Lionel Ains <lionel.ains@legrand.fr>
2015-09-24 16:41:55 +0200

    Renaming node library to ipv6_node

---
+++ src/ipv6_node.c
@@ -0,0 +1,50 @@
+/**
+ * file ipv6_node.c
+ */
+
+#include "ipv6_node.h"
+
+inline void node_id_to_ipv6(const node_id_t input, struct in6_addr* output) {
+	assert(sizeof(output->s6_addr) == sizeof(input.uint128_a8));
+
+	memcpy((void *)(output->s6_addr), (void *)(input.uint128_a8), sizeof(output->s6_addr));
+}
+
+node_id_t get_root_node_id(const self_ip_routing_tree_t* tree) {
+
+	assert(tree);
+	assert(tree->Rmax > 0);
+	assert(tree->Rmax - 1 <= 128);
+	return power2_to_uint128_t(tree->Rmax - 1);
+}
+
+rank_t node_id_to_rank(const self_ip_routing_tree_t* tree, node_id_t node) {
+	return 0;	// Lionel: TODO
+}
+
+node_id_t get_left_child_node_id(const self_ip_routing_tree_t* tree, const node_id_t parent_node) {
+	rank_t Rmax_1, R;
+	rank_t parent_rank;
+
+	//Formula: LC(p) = p - 2^(Rmax - 1 - Rp)
+	assert(tree);
+	assert(tree->Rmax > 0);
+	assert(tree->Rmax - 1 <= 128);
+	Rmax_1 = tree->Rmax - 1;
+	parent_rank = node_id_to_rank(tree, parent_node);
+	assert(parent_rank<=Rmax_1);
+	R = Rmax_1 - parent_rank;
+	return uint128_t_sub(parent_node, (node_id_t)power2_to_uint128_t(R));
+}
+
+node_id_t get_parent_node_id(self_ip_routing_tree_t tree, node_id_t child_node) {
+	node_id_t zero;
+	zero_uint128_t(zero);	//FIXME
+	return zero;
+}
+
+node_id_t get_node_id_from_parent_hostname(self_ip_routing_tree_t tree, char* parent_hostname) {
+	node_id_t zero;
+	zero_uint128_t(zero);	//FIXME
+	return zero;
+}
---
+++ src/ipv6_node.h
@@ -0,0 +1,80 @@
+/**
+ * file ipv6_node.h
+ */
+
+#ifndef __IPV6_NODE_H__
+#define __IPV6_NODE_H__
+
+#include "uint128.h"	// For uint128_t
+
+#include "utils.h"	// For assert()
+#include <string.h>	// For memcpy()
+
+typedef uint16_t rank_t;
+typedef uint8_t prefix_t;
+
+typedef uint128_t node_id_t;
+
+/**
+ * \brief IP protocol type (IPV4/IPV6)
+**/
+typedef enum {
+	  IPV4 = 0,
+	  IPV6,
+} ip_protocol_t;
+
+/**
+ * \brief Specification of a self creating IP routing tree
+**/
+typedef struct {
+  ip_protocol_t  ip_type; /*!< Type of IP addresses represented by the tree (IPv4, IPv6) */
+  rank_t         Rmax; /*!< Maximum depth of the tree */
+  prefix_t       treeP; /*!< IP prefix for the range allocated to the tree */
+  prefix_t       hostA; /*!< Host address size in bits */
+} self_ip_routing_tree_t;
+
+/**
+ * \brief Convert a node ID to an IPv6 address represented as a struct in6_addr
+ *
+ * \param[in] input The node ID to convert
+ * \param[out] output A struct in6_addr for which s6_addr will be filled-in based on \p input (note: other fields are unchanged)
+**/
+inline void node_id_to_ipv6(const node_id_t input, struct in6_addr* output);
+
+/**
+ * \brief Get the node ID of the root node in a routed self-generated tree
+ *
+ * \param tree The tree for which we want to get the root node ID
+ *
+ * \return The node ID of the root
+ */
+node_id_t get_root_node_id(const self_ip_routing_tree_t* tree);
+
+/**
+ * \brief Get the rank associated with a given node ID in a routed self-generated tree
+ *
+ * \param tree The tree for which we want to calculate the rank
+ *
+ * \return The rank (in the tree) of the requested \p node
+ */
+rank_t node_id_to_rank(const self_ip_routing_tree_t* tree, node_id_t node);
+
+/**
+ * \brief Get the left child node ID based on a parent node ID
+ *
+ * \return The node ID or 0 if there was an error
+**/
+node_id_t get_left_child_node_id(const self_ip_routing_tree_t* tree, const node_id_t parent_node);
+
+/**
+ *
+**/
+node_id_t get_parent_node_id(self_ip_routing_tree_t tree, node_id_t child_node);
+
+/**
+ *
+**/
+node_id_t get_node_id_from_parent_hostname(self_ip_routing_tree_t tree, char* parent_hostname);
+
+
+#endif	// __IPV6_NODE_H__
