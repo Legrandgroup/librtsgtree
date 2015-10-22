c44efc8d5128290069625b9836e5f4a0dfac56e7
Lionel AINS <lionel.ains@legrand.fr>
2015-10-22 11:23:59 +0200

    Allowing varargs in FAIL() macro

--- src/test_harness.h
+++ src/test_harness.h
@@ -5,5 +5,5 @@
 #include <stdio.h>
 #define TEST_GROUP(x)
 #define TEST(group, func) void func()
-#define FAIL(message) do { fprintf(stderr, "%s():%d: ", __func__, __LINE__); fprintf(stderr, message); exit(1); } while(0);
+#define FAIL(message, ...) do { fprintf(stderr, "%s():%d: ", __func__, __LINE__); fprintf(stderr, message, ## __VA_ARGS__); exit(1); } while(0);
 #endif
